97de43b5ac2a7fa1211957b76d50ed5446106cf0
aao <aungaungoogame2@gmail.com>
2023-08-31 22:09:06 +0630

    added logo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <!-- logo -->
+    <router-link to="/" class="navbar-brand mx-3" href=""><img src="./assets/logo.png" alt="logo" width="70" height="60"></router-link>
+
+    <!-- collapse btn -->
+    <button class="navbar-toggler me-2" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- collapse section -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+    </div>
   </nav>
+  
   <router-view/>
 </template>
 
