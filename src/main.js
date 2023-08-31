931da605836c98f6bcdbb0ff52e14c80b00d7fcd
aao <aungaungoogame2@gmail.com>
2023-08-31 19:28:52 +0630

    git start

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+// main css
+import "../src/assets/css/main.css";
+
+createApp(App).use(router).mount("#app");
