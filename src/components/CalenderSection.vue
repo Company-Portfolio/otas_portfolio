ade8559a88c26173e6af021522bc671f39fabe09
aao <aungaungoogame2@gmail.com>
2023-09-18 22:56:20 +0630

    cleander ui add

--- src/components/CalenderSection.vue
+++ src/components/CalenderSection.vue
@@ -4,14 +4,62 @@
     <p class="calendertext col-9 mx-auto py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, </p>
     <div class="calenderContainer col-9 mx-auto">
         <h6 class="quicksand text-center px-5">You can make a appointment to discuss about your bussiness for free of charge!</h6>
+        <!-- start Calender -->
         <div class="cleanderBg col-12 mx-auto">
-            <p>Monday / September</p>
+            <p class="pt-3">Monday / September</p>
             <div class="d-flex justify-content-center datePicker">
-              <DatePicker v-model="date"  :expanded="expanded" isDark borderless transparent title-position="left"></DatePicker>
+              <DatePicker v-model="date"  :expanded="expanded" isDark borderless transparent></DatePicker>
             </div>
            
         </div>
+
+        <!-- start Form -->
+        <div class="formContainer p-4">
+            <p class="formheader">Make a <span>Appointment</span></p>
+            <form action="" class="forms">
+              <input type="text" class="form-control texts" placeholder="Company Name">
+              <input type="email" class="form-control texts" placeholder="Company Email">
+              <div class="radioContainer">
+                <h5 class="radioheader">Avaliable Time</h5>
+                <div class="ms-3 mt-3">
+                  <div class="form-check">
+                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                      <label class="form-check-label" for="exampleRadios1">
+                        9:00 AM - 10:00 AM
+                      </label>
+                  </div>
+                  <div class="form-check ">
+                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                      <label class="form-check-label" for="exampleRadios2">
+                        11:00 AM - 12:00 PM
+                      </label>
+                  </div>
+                  <div class="form-check">
+                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option1">
+                      <label class="form-check-label" for="exampleRadios3">
+                        2:00 PM - 3:00 PM
+                      </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group textareas mt-4">
+                <label for="exampleFormControlTextarea1">Project Decription</label>
+                <textarea class="form-control mt-2" id="exampleFormControlTextarea1" rows="5" placeholder="Please let us know what is your bussiness and what  problem you wanna solve in your bussiness...."></textarea>
+              </div>
+
+              <div class= "d-flex justify-content-center mt-3">
+                <button class="buttons" type="submit">Make Appointment</button>
+              </div>
+            </form>
+        </div>
+
+
+        <p class="quicksand mt-3">
+          A appointment meeting link will be send to your email. If you can’t find the invitation link, we suggest you to look into the spam...
+        </p>
+
     </div>
+    
   </div>
 </template>
 
