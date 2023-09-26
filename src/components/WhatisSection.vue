b26d8200e75a1ee2012a77679b43c5bf0051c907
aao <aungaungoogame2@gmail.com>
2023-09-26 22:03:31 +0630

    aboutus page finished

--- src/components/WhatisSection.vue
+++ src/components/WhatisSection.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="container-fluid ps-5 py-4">
+  <div class="container-fluid py-4">
    <div class="row">
-    <div class="col-12 col-md-6 col-lg-8 whatiscontainer">
+    <div class="col-12 col-md-6 col-lg-8 ps-5 whatiscontainer">
       <h3 class="sectionTitle mt-3">What is <span class="blues">OTAS?</span></h3>
       <p class="normalText pt-2 col-10">
         In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available
       </p>
     </div>
     <div class="col-12 col-md-6 col-lg-4">
-      <div>
+      <div class="mx-auto">
         <img src="../assets/photo/Onboarding-bro.png" alt="" width="350" height="350">
       </div>
     </div>
